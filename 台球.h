8535d5b9d7fa20da2e94534bc7c3783946bbdfca
candoanybutcoding <84218590+candoanybutcoding@users.noreply.github.com>
2021-05-29 15:54:56 +0800

    Update 台球.h

--- 台球.h
+++ 台球.h
@@ -1,18 +1,20 @@
 #ifndef _BALL___h____
 #define _BALL___h____
 
-/*
-该模块名为BALL
-*/ 
 
 
-#define BALL_RADIUS 0.3
-#define BALL_DRAWBALL_STARTING_POSITION 0 
-#define BALL_DRAWBALL_ENDING_POSITION 360
-#define BALL_GOAL_IN 1 
-#define BALL_GOAL_OUT -1
-#define BALL_FORWORD 1
-#define BALL_BACKWORD -1
+
+
+#define BALL_DRAWARC_LINEWIDTH 0.002
+#define BALL_DRAWARC_RADIUS_CHANGE 0.0005
+#define BALL_ACCELERATION  0.0005
+#define BALL_BROWN_R 0.35
+#define BALL_BROWN_G 0.20
+#define BALL_BROWN_B 0.05
+#define BALL_PINK_R 1
+#define BALL_PINK_G 0.6
+#define BALL_PINK_B 0.8
+
 
 
 typedef char char_t; 
@@ -28,73 +30,63 @@ typedef float float32_t;
 typedef double float64_t; 
 typedef long double float128_t; 
 
-/*
-unsigned  u
-char_t     c
-int16_t    s
-int32_t    l
-int64_t    ll
-bool_t     b
-float32_t  fl
-float64_t  d
-float128_t ld
-数组      a
-指针      p
-结构体    st
-枚举      en
-函数      f
-*/
 
 typedef enum {
-	
-	BALL_FORWORD, BALL_BACKWORD
-	
-} BALL_Direction_E; /*朝x,y轴正向为FORWARD,负向为BACKWARD*/ 
+ BALL_FORWARD = 1, BALL_BACKWARD = -1
+} BALL_Direction_E; 
 
 typedef enum{
-	
-	
+ BALL_RED, BALL_YELLOW, BALL_GREEN, BALL_BROWN, BALL_BLUE,
+ BALL_PINK, BALL_BLACK, BALL_WHITE, BALL_GRAY
 } BALL_Color_E;
 
 typedef enum {
-	
-	BALL_GOAL_IN，BALL_GOAL_OUT 
-	
-} BALL_Goal_E; /*判断球是否进洞*/ 
+ BALL_GOAL_IN = 0, BALL_GOAL_OUT = 1
+} BALL_Goal_E; 
 
 
 
 
 
 
-struct Ball{
-	
-	float64_t d_position_x;
- 	float64_t d_position_y;/*球的x,y坐标*/
- 	float64_t d_speed_x;
- 	float64_t d_speed_y;/*球的x,y上的速率*/
- 	BALL_Direction_E l_direction_x;
- 	BALL_Direction_E l_direction_y;/*球的速度对于x,y轴的方向*/
- 
+struct BALL_Ball_S{
+ float64_t d_radius;
+   float64_t d_position_x;
+   float64_t d_position_y;
+   float64_t d_speed_x;
+   float64_t d_speed_y;
+   BALL_Direction_E l_direction_x;
+   BALL_Direction_E l_direction_y;
+  BALL_Goal_E l_ifgoal;
+  BALL_Color_E l_color;
 };
 
-typedef struct Ball* BALL_pBall_s;
+typedef struct BALL_Ball_S* BALL_Ball_PS;
 
 
-
-void Draw_Single_Ball (BALL_pBall_s BALL_ID);/*画出一个球*/
-
-
-void dMove_Single_Ball (BALL_pBall_s BALL_ID);/*计算这一帧内的位移和摩擦对速度的变化*/
+void Draw_Single_Ball (BALL_Ball_PS pst_BALL_ID);
 
 
-void Hit_Wall_Single_Ball (BALL_pBall_s BALL_ID);/*计算一次撞墙后速度的变化*/
+void dMove_Single_Ball (BALL_Ball_PS pst_BALL_ID);
 
 
-BALL_Direction_E Direction_Change (BALL_Direction_E temp_direction);/*改变方向的正负*/ 
+void Hit_Wall_Single_Ball (BALL_Ball_PS pst_BALL_ID);
 
 
-void Collided_Single_Ball (BALL_pBall_s BALL_ID_1, BALL_pBall_s BALL_ID_2);/*计算两球相撞后两个球的速度*/
+BALL_Direction_E Direction_Change (BALL_Direction_E temp_direction);
+
+
+void Collided_Single_Ball (BALL_Ball_PS pst_BALL_ID_1, BALL_Ball_PS pst_BALL_ID_2);
+
+
+void Check_Goal_Ball(BALL_Ball_PS pst_BALL_ID);
+
+
+void Reset_Ball_Position(BALL_Ball_PS pst_BALL_ID);
+
+
+float64_t Distance_TowPoints(float64_t d_x1, float64_t d_y1, float64_t d_x2, float64_t d_y2);
+
 
 
 #endif
